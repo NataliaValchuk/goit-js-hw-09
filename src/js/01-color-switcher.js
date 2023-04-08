868549e25953159b348d1692bfbdcaf305f89d64
NataliaValchuk <trewax321@gmail.com>
2023-04-08 19:57:57 +0300

    added src files

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let intervalId;
+
+function getRandomHexColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function startColorSwitcher() {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function stopColorSwitcher() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+startBtn.addEventListener('click', startColorSwitcher);
+stopBtn.addEventListener('click', stopColorSwitcher);
+
